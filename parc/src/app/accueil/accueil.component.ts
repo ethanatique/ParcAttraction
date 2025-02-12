df5fdc62200d950054ee429b01e7b0d78fd1ca25
ethanatique <65488662+ethanatique@users.noreply.github.com>
2025-02-12 09:48:36 +0100

    Ergonomie page acceuil

--- parc/src/app/accueil/accueil.component.ts
+++ parc/src/app/accueil/accueil.component.ts
@@ -6,18 +6,30 @@ import { AttractionInterface } from '../Interface/attraction.interface';
 import { MatCardModule } from '@angular/material/card';
 import { MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';  // Importation nécessaire pour mat-icon
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [CommonModule, MatCardModule,MatButton,RouterLink],
+  imports: [CommonModule, MatCardModule, MatButton, RouterLink, MatIconModule],  // Ajout de MatIconModule ici
   templateUrl: './accueil.component.html',
-  styleUrl: './accueil.component.scss'
+  styleUrls: ['./accueil.component.scss']
 })
 export class AccueilComponent {
+  public attractions: Observable<AttractionInterface[]> = this.attractionService.getAllAttraction();
 
-  constructor(public attractionService: AttractionService)
-  {}
-  
-  public attractions: Observable<AttractionInterface[]> = this.attractionService.getAllAttraction()
+  constructor(public attractionService: AttractionService) {}
+
+  // Méthode pour générer les étoiles en fonction de la difficulté
+  getStars(difficulty: number): string[] {
+    const stars = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < difficulty) {
+        stars.push('star');  // Etoile pleine
+      } else {
+        stars.push('star_border');  // Etoile vide
+      }
+    }
+    return stars;
+  }
 }
